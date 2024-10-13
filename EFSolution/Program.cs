d7a78ddc9cad1ab49417710059733b817f6dde36
Yassin <201800011@post.au.dk>
2024-10-13 19:40:40 +0200

    Added namespace for program.cs and removed weatherforecast files

--- EFSolution/Program.cs
+++ EFSolution/Program.cs
@@ -1,30 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
+using EFSolution.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-// DbContext service
-builder.Services.AddDbContext<FoodAppContext>(options =>
+namespace EFSolution
 {
-    options.UseSqlServer(builder.Configuration["FoodAppContext"]);
-});
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+            // Add services to the container.
+            var conn = builder.Configuration["ConnectionStrings:DefaultConnection"];
+            builder.Services.AddDbContext<FoodAppContext>(options =>
+            {
+                options.UseSqlServer(conn); // Ensure you use the correct connection string
+            });
+            builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
