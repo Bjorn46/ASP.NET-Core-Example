ccbce82436358c7b03ae0cb8fc99e8b9a5824bf2
Yassin <201800011@post.au.dk>
2024-11-15 11:27:24 +0100

    Removed EFsolution

--- EFSolution/DTOs/Assignment2BDtos/AverageRatingDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment2.DTOs.AssignmentBQueries
-{
-    public class AverageRatingDto
-    {
-        public double Average_Rating { get; set; }
-    }
-
-
-}
--- EFSolution/DTOs/Assignment2BDtos/DeliveryDishDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment2.DTOs.AssignmentBQueries
-{
-    public class DeliveryDishDTO
-    {
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public string CookName { get; set; }
-    }
-}
